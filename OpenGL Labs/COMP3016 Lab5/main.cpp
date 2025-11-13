9bb59802096c0a4f924c3aaf988aca3bb4dc0011
Jack Dixon <jack.dixon@students.plymouth.ac.uk>
2025-11-13 17:15:16 +0000

    Lab 5 work

--- OpenGL Labs/COMP3016 Lab5/main.cpp
+++ OpenGL Labs/COMP3016 Lab5/main.cpp
@@ -1,8 +1,152 @@
+//STD
+#include <iostream>
+
+//GLEW
 #include <GL/glew.h>
+
+//GLFW
 #include <GLFW/glfw3.h>
+
+#include "shaders/LoadShaders.h"
+
+GLuint program;
+
+using namespace std;
+
+// Callback function for window resizing
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // Update the OpenGL viewport
+    glViewport(0, 0, width, height);
+}
+
+void ProcessUserInput(GLFWwindow* WindowIn)
+{
+    //Closes window on 'exit' key press
+    if (glfwGetKey(WindowIn, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(WindowIn, true);
+    }
+}
+
+//VAO vertex attribute positions in correspondence to vertex attribute type
+enum VAO_IDs { Triangles, Indices, Colours, Textures, NumVAOs = 2 };
+//VAOs
+GLuint VAOs[NumVAOs];
+
+//Buffer types
+enum Buffer_IDs { ArrayBuffer, NumBuffers = 4 };
+//Buffer objects
+GLuint Buffers[NumBuffers];
 
 int main()
 {
-    // You can put the OpenGL initialisation here later.
+    // ----- Initialize GLFW -----
+    if (!glfwInit())
+    {
+        cout << "Failed to initialize GLFW\n";
+        return -1;
+    }
+
+    // ----- Create Window -----
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Lab5", NULL, NULL);
+
+    if (window == NULL)
+    {
+        cout << "GLFW Window did not instantiate\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    // Bind OpenGL context to this window
+    glfwMakeContextCurrent(window);
+
+    // ----- Initialize GLEW -----
+    if (glewInit() != GLEW_OK)
+    {
+        cout << "Failed to initialize GLEW\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    //Load shaders
+    ShaderInfo shaders[] =
+    {
+        { GL_VERTEX_SHADER, "shaders/vertexShader.vert" },
+        { GL_FRAGMENT_SHADER, "shaders/fragmentShader.frag" },
+        { GL_NONE, NULL }
+    };
+
+    program = LoadShaders(shaders);
+    glUseProgram(program);
+
+    // Set the initial viewport size
+    glViewport(0, 0, 1280, 720);
+
+    // Set the callback for window resizing
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    float vertices[] = {
+    -0.5f, -0.5f, 0.0f, //pos 0 | x, y, z
+    0.5f, -0.5f, 0.0f, //pos 1
+    0.0f, 0.5f, 0.0f //pos 2
+    };
+
+    //Declaration of index of VBO
+    unsigned int vertexBufferObject;
+    //Sets index of VBO
+    glGenBuffers(1, &vertexBufferObject);
+    //Binds VBO to array buffer for drawing vertices
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferObject);
+    //Allocates buffer memory for the vertices
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    //Allocates vertex attribute memory for vertex shader
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    //Index of vertex attribute for vertex shader
+    glEnableVertexAttribArray(0);
+
+    //Sets index of VAO
+    glGenVertexArrays(NumVAOs, VAOs);
+    //Binds VAO to a buffer
+    glBindVertexArray(VAOs[0]);
+    //Sets indexes of all required buffer objects
+    glGenBuffers(NumBuffers, Buffers);
+
+    //Binds VAO to array buffer
+    glBindBuffer(GL_ARRAY_BUFFER, Buffers[Triangles]);
+    //Allocates buffer memory for the vertices
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    //Allocates vertex attribute memory for vertex shader
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    //Index of vertex attribute for vertex shader
+    glEnableVertexAttribArray(0);
+
+    //Unbinding
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // ----- Main Loop -----
+    while (!glfwWindowShouldClose(window))
+    {
+        //Input
+        ProcessUserInput(window); //Takes user input
+
+        //Rendering
+        glClearColor(0.25f, 0.0f, 1.0f, 1.0f); //Colour to display on cleared window
+        glClear(GL_COLOR_BUFFER_BIT); //Clears the colour buffer
+
+        glBindVertexArray(VAOs[0]); //Bind buffer object to render
+        glDrawArrays(GL_TRIANGLES, 0, 3); //Render buffer object
+
+        //Refreshing
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // ----- Cleanup -----
+    glfwTerminate();
     return 0;
 }
+
